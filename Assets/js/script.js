38bfc7b9a565f3137850bc8676cba704e0dbc09c
Holy Paul <brijeeta@gmail.com>
2021-09-12 19:20:21 -0400

    added proper comments in html and js file for table

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
+// variables
 var baseURL = "https://api.coingecko.com/api/v3/";
-var table = $('.table');
 var tableBody = $('tbody');
+// usd currency formatter
 var formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
@@ -10,26 +11,28 @@ var formatter = new Intl.NumberFormat('en-US', {
 
 getPublicTreasury(baseURL);
 
+// async function to call public treasury API to fetch details of companies holding bitcoin
 async function getPublicTreasury(url) {
 
     var url = baseURL + "companies/public_treasury/bitcoin"
     var response = await fetch(url);
     var data = await response.json();
-    console.log(data);
+    // console.log(data);
     var companies = data.companies;
-    console.log(companies);
+    // console.log(companies);
 
     var tableString = "<tr>"
-
+        // for each loop to iterate companies array
     companies.forEach(element => {
+        // for loop to iterate object values inside companies array
         for (var prop in element) {
             console.log("props: " + prop);
             tableString += `<td>${element[prop]}</td>`
         }
         tableString += "</tr>"
     });
-
-    console.log(tableString);
+    // console.log(tableString);
+    // append the data rows dynamically to tbody element in html
     tableBody.append(tableString);
 
 }
